fc6d3c9a09945b8b561774c79eba4714dcda34e2
olawale-o <omoogunolawale@gmail.com>
2021-07-22 10:41:58 +0100

    Create dom manipulation  test cases for add and remove task to and from the dom

--- src/add_remove_seperated.js
+++ src/add_remove_seperated.js
@@ -1,13 +1,13 @@
 export const addNewTodo = (task, todos) => {
-    const newTodo = {
-      index: todos.length < 1 ? 1 : todos.length + 1,
-      description: task,
-      completed: false,
-    };
-    return newTodo;
+  const newTodo = {
+    index: todos.length < 1 ? 1 : todos.length + 1,
+    description: task,
+    completed: false,
   };
-  
-  export const onDeleteTodo = (target, todos) => {
-    todos = todos.filter((todo) => target !== todo.index);
-    return todos;
-  };
+  return newTodo;
+};
+
+export const onDeleteTodo = (target, todos) => {
+  todos = todos.filter((todo) => target !== todo.index);
+  return todos;
+};
